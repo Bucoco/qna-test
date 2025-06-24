2248f1c767bb690c911b8149927922cd0bb63692
Bucoco <nam2660433@gmail.com>
2025-06-24 12:26:03 +0900

    test commit

--- src/main/java/com/lg2/qna_service/QnaServiceApplication.java
+++ src/main/java/com/lg2/qna_service/QnaServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class QnaServiceApplication {
-
+	//주석
 	public static void main(String[] args) {
 		SpringApplication.run(QnaServiceApplication.class, args);
 	}
